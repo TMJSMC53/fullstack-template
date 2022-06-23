a0952cb4bf8ec294213553eb5971c29460b452a9
Triola Jackson <triolajackson@gmail.com>
2022-06-22 19:44:03 -0500

    add port and mongodb connections

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ MongoClient.connect(dbConnectionString).then((client) => {
   db = client.db(dbName);
   collection = db.collection("alien-info");
 });
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
